5bfb2ee91be44199bd3b89430a542e7354a9ca6b
savinaa <savin4eto_92@mail.bg>
2019-05-15 23:47:50 +0300

    processing logic implemented

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,59 @@
-
+import java.util.Scanner;
 
 public class Main {
+    public static String SPACE_SYMBOL = "-";
+    public static String FIGURE_SYMBOL = "*";
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+        int n = Integer.parseInt(input.nextLine());
+        
+        while (n % 2 == 0 || (n <= 2 || n > 9999)) {
+            System.out.println("Unexpected value found! Please enter an ODD number 2 < N < 10000: ");
+            n = Integer.parseInt(input.nextLine());
+        }
+        
+        int figureHeight = n + 1;
+        int figureHalf = figureHeight / 2;
+        int halfOfN = n / 2;
+        
+        //upper part
+        for (int i = n, j = 0; i >= figureHalf; i--, j++) {
+            String leftHalf = repeatString(SPACE_SYMBOL, i) +
+                    repeatString(FIGURE_SYMBOL, n + j) +
+                    repeatString(FIGURE_SYMBOL, j) +
+                    repeatString(SPACE_SYMBOL, halfOfN - j) + "-";
+            
+            String rightHalf = reverseString(leftHalf);
+            System.out.println(repeatString(leftHalf + rightHalf,2));
+        }
+
+        //bottom part
+        for (int i = figureHalf, j = 0; i > 0; i--, j++) {
+            String leftHalf = repeatString(SPACE_SYMBOL, halfOfN-j) +
+                    repeatString(FIGURE_SYMBOL, n ) +
+                    repeatString(SPACE_SYMBOL, j) +
+                    "-"+
+                    repeatString(SPACE_SYMBOL,  j) +
+                    repeatString(FIGURE_SYMBOL,  n-j-1) +"*";
+            String rightHalf = reverseString(leftHalf);
+            System.out.println(repeatString(leftHalf + rightHalf,2));
+        }
+
+
+    }
+
+    private static String reverseString(String leftHalf) {
+        StringBuilder sb = new StringBuilder(leftHalf);
+        sb.reverse();
+        return sb.toString();
+    }
+
+    public static String repeatString(String str, int count) {
+        StringBuilder seq = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            seq.append(str);
+        }
+        return seq.toString();
     }
 }
